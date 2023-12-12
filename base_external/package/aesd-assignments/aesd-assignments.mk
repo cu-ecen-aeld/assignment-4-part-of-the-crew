4fd0a75a152e1b76f74843b25ea0fa1e3a74c213
part-of-the-crew <nikolai.dunichev@gmail.com>
2023-12-12 12:05:55 +0200

    for A7

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -10,9 +10,14 @@ AESD_ASSIGNMENTS_VERSION = e955a98b1bb1c6006222f3e34ca00639050fe65e
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
+
 AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignment-1-part-of-the-crew.git
+
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
+
+
+
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
@@ -34,7 +39,6 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/etc/init.d/S99aesdsocket	
 	
 endef
-
 
 
 $(eval $(generic-package))
