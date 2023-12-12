0c380dbeb153140ea74ccbbf869d797b03eb83bc
part-of-the-crew <nikolai.dunichev@gmail.com>
2023-12-12 15:22:01 +0200

    A7_2

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-LDD_VERSION = 5dd3dceedc6072aa172a0af4e0c136c94381020d
+LDD_VERSION = 133747bc4812eb65abfa0a216bc66d21ed512169
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -15,9 +15,9 @@ LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-part-of-the-crew.git
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
 
-AESD_ASSIGNMENTS_MODULE_SUBDIRS = misc-modules scull
+LDD_MODULE_SUBDIRS = misc-modules scull
 
-AESD_ASSIGNMENTS_MODULE_MAKE_OPTS = KVERSION=$(LINUX_VERSION_PROBED)
+LDD_MODULE_MAKE_OPTS = KVERSION=$(LINUX_VERSION_PROBED)
 
 define LDD_BUILD_CMDS
 	#$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/misc-modules all
